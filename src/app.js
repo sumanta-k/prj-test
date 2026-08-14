89c0dd017dddcaefd414e43f14892cf782c2f547
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 14:33:09 +0530

    FEAT: add basic config
    - add support for json formatted data to be accepted by the server
      (limit: 16kb)
    - add urlencoded
    - add limit to 16kb

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+// basic config
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+
 app.get('/', (req, res) => {
   res.send('Homepage');
 });
