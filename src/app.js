8e3baac0867ea919b503cc2bd8f455d4b340e529
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 14:48:07 +0530

    chore: add allowedHeaders
    - tells what kind of headers are being accepted

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cors());
     methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
 
 app.get('/', (req, res) => {
   res.send('Homepage');
