45c8d407796d3e18ef6f30d15eb411bc41af5949
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 14:27:47 +0530

    chore: remove express initialization
    - remove routes which are being used to serve on the path "/" and
      "/instagram"
    - paths are moved to app.js file

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 const PORT = process.env.PORT || 8000;
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the home page');
-});
-
-app.get('/instagram', (req, res) => {
-  res.send('Here We go on the instagram page');
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
