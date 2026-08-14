f1ae462830837813de5e9e664dc37007d1ffcc26
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 14:50:34 +0530

    chore: cors function style update

--- src/app.js
+++ src/app.js
@@ -6,11 +6,14 @@ const app = express();
 // basic config
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-app.use(cors());
+app.use(
+  cors({
     methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Authorization', 'Content-Type'],
     credentials: true,
     origin: process.env.ORIGIN?.split(',') || 'http://127.0.0.1:5173'
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Homepage');
