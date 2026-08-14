1cfa7cfbc7314a11572eb051562a571de4caa120
sumanta <mohantasumanta601@gmail.com>
2026-08-14 14:09:05 +0530

    Log the port being used on startup

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 
 let portBeingUsed = process.env.PORT || 8000;
+
+console.log("used port is " + portBeingUsed);
 console.log('backend get started');
