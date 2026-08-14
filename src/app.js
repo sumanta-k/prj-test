3bbc3603d5001a98c0eb57bdee1e4cdf324ff3e1
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 14:47:15 +0530

    chore: add default cors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 // basic config
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Homepage');
