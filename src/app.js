2991d6752e4cb1f51189fe5e2c1ca49ce01b6cc5
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 14:47:42 +0530

    chore: add methods
    - tells which method to allow

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cors());
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
 
 app.get('/', (req, res) => {
   res.send('Homepage');
