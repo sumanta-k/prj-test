7fd80ccd07d8dbda351a670c9e773fea5a2dc8df
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 20:30:44 +0530

    FEAT: connect db first and after it run server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import { connectDB } from './db/index.js';
 
 dotenv.config({ path: './.env' });
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://127.0.0.1:${PORT}`);
-});
+connectDB()
+  .then(
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://127.0.0.1:${PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.error('having issue while connecting to db ' + err);
+    process.exit(1);
+  });
