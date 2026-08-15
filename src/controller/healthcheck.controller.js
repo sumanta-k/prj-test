3e74c10eb4199926eb18beed443cc5064cbe998d
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-15 15:29:53 +0530

    FIX: solves sending response and errors without too much engaging with try..catch

--- src/controller/healthcheck.controller.js
+++ src/controller/healthcheck.controller.js
@@ -1,14 +1,3 @@
 import { ApiResponse } from '../utils/api-response.js';
 
-const healthCheckController = async (req, res, next) => {
-  try {
-    await getUserFromDB(); // calling db to get user
-    res
-      .status(200)
-      .json(new ApiResponse(200, { data: 'health check is running' }));
-  } catch (err) {
-    next(err); //handling errors by express given error handler
-  }
-};
-
 export { healthCheckController };
