47daf01b6257ef379e0634e2f2aa324eff482769
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-15 15:47:27 +0530

    refactor: add healthCheckController to response errors with better way

--- src/controller/healthcheck.controller.js
+++ src/controller/healthcheck.controller.js
@@ -1,3 +1,15 @@
 import { ApiResponse } from '../utils/api-response.js';
+import { asyncHandler } from '../utils/async-handler.js';
 
+const healthCheckController = asyncHandler(async (req, res, next) => {
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { data: 'health check is ok' },
+        "ding dong it's working"
+      )
+    );
+});
 export { healthCheckController };
