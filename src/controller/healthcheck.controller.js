b817902922b4bf80795856015e87a9f92328ca13
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-15 15:07:14 +0530

    perf: handles error more efficiently with the help of express

--- src/controller/healthcheck.controller.js
+++ src/controller/healthcheck.controller.js
@@ -1,11 +1,14 @@
 import { ApiResponse } from '../utils/api-response.js';
 
-const healthCheckController = (req, res) => {
+const healthCheckController = async (req, res, next) => {
   try {
+    await getUserFromDB(); // calling db to get user
     res
       .status(200)
       .json(new ApiResponse(200, { data: 'health check is running' }));
-  } catch (err) {}
+  } catch (err) {
+    next(err); //handling errors by express given error handler
+  }
 };
 
 export { healthCheckController };
