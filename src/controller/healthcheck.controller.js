0fd2b35c34972f23989151bb9ca9d0378cad506e
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-15 09:54:31 +0530

    chore: rename healthCheck to healthCheckController

--- src/controller/healthcheck.controller.js
+++ src/controller/healthcheck.controller.js
@@ -1,11 +1,11 @@
 import { ApiResponse } from '../utils/api-response.js';
 
-const healthCheck = (req, res) => {
+const healthCheckController = (req, res) => {
   try {
-    res.status(200).json({
-      new ApiResponse(200,{"data":"health check is running"});
-    })
+    res
+      .status(200)
+      .json(new ApiResponse(200, { data: 'health check is running' }));
   } catch (err) {}
 };
 
-export {healthCheck};
+export { healthCheckController };
