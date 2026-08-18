bc8b25b0bbd941c72ad51eec1b6a9c7eb3f93edb
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-18 10:51:29 +0530

    feat: generate refresh token for user

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -49,4 +49,10 @@ userSchema.methods.generateAccessToken = function () {
     { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
   );
 };
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+  });
+};
 const User = mongoose.model('user', userSchema);
