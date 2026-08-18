ab587d63d0758e65cfaa42cd3f39e722343088c0
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-18 10:47:47 +0530

    feat: generate access token for user

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -39,4 +39,13 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password); // [true or false]
 };
+
+// generate access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id, username: this.username, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
 const User = mongoose.model('user', userSchema);
