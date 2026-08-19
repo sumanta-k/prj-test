57d607356c1845da90d56d33fdcc698c05abddfd
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-19 11:15:55 +0530

    feat: send email to user

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,40 @@
 import Mailgen from 'mailgen';
+import nodemailer from 'nodemailer';
+
+const sendMail = async (options) => {
+  // first create mailgen instance
+  const mailGenerator = new Mailgen({
+    theme: 'default',
+    product: { name: 'Test Platform', link: 'https://testPlatformExample.com' }
+  });
+
+  // create email body for simple text
+  const emailTextForm = mailGenerator.generatePlaintext(options.mailgenContent);
+  // create email body for html
+  const emailHtmlForm = mailGenerator.generate(options.mailgenContent);
+
+  // now create transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASSWORD
+    }
+  });
+
+  try {
+    await transporter.sendMail({
+      from: '"Test Platform" <teamTest@example.com>', // sender address
+      to: options.email,
+      subject: options.subject,
+      text: emailTextForm,
+      html: emailHtmlForm
+    });
+  } catch (err) {
+    console.error('error while sending email', err);
+  }
+};
 
 const userVerificationMailgenContent = (username, verificationURL) => {
   return {
