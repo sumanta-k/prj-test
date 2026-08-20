7bd8d0355bf7583958b2aee391719a7dd125c4f0
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-20 17:20:02 +0530

    refactor: change position

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -2,8 +2,8 @@ class ApiError extends Error {
   constructor(
     statusCode,
     message = 'Something Went Wrong',
-    stack = '',
-    errors = []
+    errors = [],
+    stack = ''
   ) {
     this.statusCode = statusCode;
     this.message = message;
